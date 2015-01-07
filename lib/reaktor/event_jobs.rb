da360ff06f4d8c4199b9c69687aa75628fb17514
Greg Saramite <g.saramite@gmail.com>
2015-01-07 11:52:57 -0800

    added sync_only_event to module EventJobs

--- lib/reaktor/event_jobs.rb
+++ lib/reaktor/event_jobs.rb
@@ -4,6 +4,7 @@ module Reaktor
     require 'jobs/create_event'
     require 'jobs/delete_event'
     require 'jobs/modify_event'
+    require 'jobs/sync_only_event'
     require 'jobs/controller'
     require 'jobs/github_controller'
     require 'jobs/gitlab_controller'
