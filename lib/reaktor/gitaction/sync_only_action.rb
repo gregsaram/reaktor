f740dce7b61a7ed85211ee9caf56f68c6197dbe9
Greg Saramite <g.saramite@gmail.com>
2015-01-07 16:01:26 -0800

    GitAction::SyncOnlyAction - harcoded hiera prefix for r10k_deploy_env

--- lib/reaktor/gitaction/sync_only_action.rb
+++ lib/reaktor/gitaction/sync_only_action.rb
@@ -11,11 +11,12 @@ module GitAction
       logger.info("branch = #{self.branch_name}")
     end
     
-    def sync     
-      Notification::Notifier.instance.notification = "r10k deploy environment for #{branch_name} in progress..."
-      result = r10k_deploy_env self.branch_name
+    def sync
+           
+      Notification::Notifier.instance.notification = "r10k deploy environment for hiera_#{branch_name} in progress..."
+      result = r10k_deploy_env 'hiera_' + self.branch_name
       if result.exited?
-        Notification::Notifier.instance.notification = "r10k deploy environment for #{branch_name} finished"
+        Notification::Notifier.instance.notification = "r10k deploy environment for hiera_#{branch_name} finished"
       end
     end
 end
